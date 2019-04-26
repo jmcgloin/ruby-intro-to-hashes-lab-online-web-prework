117e1b439021186810574a0d07df351cc40c7db2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 17:54:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,9 @@ end
 
 def monopoly_with_second_tier
 	railroads => {
-	    :pieces => 4,
-	    :names => {},
-	    :rent_in_dollars=> {}
+	    :pieces = 4,
+	    :names = {},
+	    :rent_in_dollars = {}
 	  }
 	
 end
