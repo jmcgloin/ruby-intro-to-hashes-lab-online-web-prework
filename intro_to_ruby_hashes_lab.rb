b7b7947ff7e6e46418b078671476fa29a1ab0ede
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 18:00:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,20 +7,10 @@ def actor
 end
 
 def monopoly
-# 	monopoly = {
-# 	  railroads = {}
-# 	}
-
 end
 
 def monopoly_with_second_tier
-	monopoly = {
-	  railroads = {
-	    pieces: 4,
-	    names: {},
-	    rent_in_dollars: {}
-	  }
-	}
+	
 	
 end
 
